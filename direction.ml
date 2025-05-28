f0acd699006353af2231b8bdeda5ef4d8226ce1f
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-05-28 19:17:07 +0200

    feat: direction: add offset_of_direction

--- direction.ml
+++ direction.ml
@@ -19,3 +19,15 @@ let direction_of_tile = function
   | NorthWest -> 7
 
 let compare direction1 direction2 = Int.compare (direction_of_tile direction1) (direction_of_tile direction2)
+
+let offset_of_direction = function
+  | North -> (-1, 0)
+  | NorthEast -> (-1, 1)
+  | East -> (0, 1)
+  | SouthEast -> (1, 1)
+  | South -> (1, 0)
+  | SouthWest -> (1, -1)
+  | West -> (0, -1)
+  | NorthWest -> (-1, -1)
+
+let all = [North; NorthEast; East; SouthEast; South; SouthWest; West; NorthWest;]
