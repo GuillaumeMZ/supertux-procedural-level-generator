4a67ce0a360314523bb4e9afddd0839d7e12d090
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-05-28 15:44:32 +0200

    feat: tile is now usable as a key type for set/map

--- tile.ml
+++ tile.ml
@@ -12,3 +12,20 @@ type t =
   | SnowBottomLeft
   | SnowBottomMiddle
   | SnowBottomRight
+
+let int_of_tile = function
+  | Air -> 0
+  | SnowLayerLeft -> 1
+  | SnowLayerMiddle -> 2
+  | SnowLayerRight -> 3
+  | SnowGroundLeft -> 4
+  | SnowGroundMiddle -> 5
+  | SnowGroundRight -> 6
+  | SnowWallLeft -> 7
+  | SnowInner -> 8
+  | SnowWallRight -> 9
+  | SnowBottomLeft -> 10
+  | SnowBottomMiddle -> 11
+  | SnowBottomRight -> 12
+
+let compare tile1 tile2 = Int.compare (int_of_tile tile1) (int_of_tile tile2)
