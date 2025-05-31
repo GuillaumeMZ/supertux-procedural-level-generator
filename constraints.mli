1193c8d531295aa7a4fd8ec0c24dd27460b4e4d5
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-05-31 12:42:04 +0200

    chore: add documentation to the constraints module

--- constraints.mli
+++ constraints.mli
@@ -1,9 +1,14 @@
 type t
 
+(** Creates a new, empty constraint map. *)
 val empty: unit -> t
 
+(** [add_constraint constraints source_tile direction target_tile] adds a constraint to [constraints], saying that
+there can be a [target_tile] in the direction [direction] from a [source_tile]. *)
 val add_constraint: t -> Tile.t -> Direction.t -> Tile.t -> t
 
+(** Creates a constraint map from a tilemap. *)
 val of_tilemap: Tile.t Grid.t -> t
 
+(** Prints a constraint map to stdout. *)
 val print: t -> unit
