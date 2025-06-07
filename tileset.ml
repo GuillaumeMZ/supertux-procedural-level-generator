b09b57d9bb80afe2ab68cc92e09b1de21d9ff164
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-06-07 22:16:27 +0200

    feat: add Tileset.to_string

--- tileset.ml
+++ tileset.ml
@@ -1,3 +1,9 @@
 include Set.Make(Tile)
 
 let all = of_list Tile.all
+
+let to_string tileset = "{" ^ (
+  fold (fun tile result ->
+    result ^ " " ^ Tile.to_string tile
+  ) tileset ""
+) ^ "}\n"
