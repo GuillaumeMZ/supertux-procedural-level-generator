ebd8652685a573a008fd01c15331dcf3bbf5b272
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-06-12 18:07:13 +0200

    feat: add level serialization

---
+++ level_serializer.ml
@@ -0,0 +1,53 @@
+let cell_grid_to_string cell_grid =
+  List.fold_left (fun block y ->
+    let current_line = List.fold_left (fun line x ->
+      match Grid.get y x cell_grid with
+      | Wfc.Uncollapsed _ -> failwith "Attempting to convert an uncollapsed cell to a string."
+      | Wfc.Collapsed tile -> line ^ " " ^ (Tile.to_int tile |> string_of_int)
+    ) block (List.init (Grid.width cell_grid) Fun.id)
+    in current_line ^ "\n"
+  ) "" (List.init (Grid.height cell_grid) Fun.id)
+
+let serialize_level cell_grid output_path =
+  let out_channel = open_out output_path in
+  Printf.fprintf out_channel {|
+    (supertux-level
+      (version 3)
+      (name (_ "Unknown"))
+      (author "Unknown")
+      (license "CC-BY-SA 4.0 International")
+      (sector
+        (name "main")
+        (ambient-light
+          (color 1 1 1)
+        )
+        (background
+          (color 1 1 1)
+          (speed 0.5)
+          (image "images/background/antarctic/arctis2.png")
+        )
+        (camera
+          (name "Camera")
+          (mode "normal")
+        )
+        (gradient
+          (top_color 0.3 0.4 0.75)
+          (bottom_color 1 1 1)
+          (z-pos -301)
+        )
+        (music
+          (file "antarctic/chipdisko.music")
+        )
+        (tilemap
+          (solid #t)
+          (z-pos 0)
+          (width %d)
+          (height %d)
+          (tiles
+            %s
+          )
+        )
+      )
+    )
+  |} (Grid.width cell_grid) (Grid.height cell_grid) (cell_grid_to_string cell_grid);
+  flush out_channel
--- splg.ml
+++ splg.ml
@@ -12,7 +12,7 @@ let () =
     [| Air; Air; Air; Air; Air |];
   |] in
   let constraint_map = Constraints.of_tilemap tilemap in
-  let wfc_grid = Grid.make 15 15 (Uncollapsed Tileset.all) in 
+  let wfc_grid = Grid.make 30 60 (Uncollapsed Tileset.all) in 
   let rec wave_function_collapse () =
     let min_entropy_cell = select_most_suitable_uncollapsed_cell wfc_grid in
     (match min_entropy_cell with
@@ -24,4 +24,4 @@ let () =
     match get_grid_state wfc_grid with
       | Finished | Invalid_cell -> ()
       | In_progress -> wave_function_collapse ()
-  in wave_function_collapse (); print_collapsed_grid wfc_grid
+  in wave_function_collapse (); Level_serializer.serialize_level wfc_grid "./level1.stl"
