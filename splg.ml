931588d85fb8f35912b84a9ed9c9db621d9c8f70
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2025-06-12 16:05:14 +0200

    feat: main wave function collapse loop, working :tada:

--- splg.ml
+++ splg.ml
@@ -1,20 +1,27 @@
 open Tile
+open Wfc
 
-let () = 
+let () =
+  Random.self_init ();
   let tilemap = Grid.of_array_of_arrays [|
-    [|SnowLayerLeft; SnowLayerMiddle; SnowLayerRight|];
-    [|SnowGroundLeft; SnowGroundMiddle; SnowGroundRight|];
-    [|SnowWallLeft; SnowInner; SnowWallRight|];
-    [|SnowBottomLeft; SnowBottomMiddle; SnowBottomRight|];
+    [| Air; Air; Air; Air; Air |];
+    [| Air; SnowLayerLeft; SnowLayerMiddle; SnowLayerRight; Air |];
+    [| Air; SnowGroundLeft; SnowGroundMiddle; SnowGroundRight; Air |];
+    [| Air; SnowWallLeft; SnowInner; SnowWallRight; Air |];
+    [| Air; SnowBottomLeft; SnowBottomMiddle; SnowBottomRight; Air |];
+    [| Air; Air; Air; Air; Air |];
   |] in
-  let constraints = Constraints.of_tilemap tilemap in
-  Constraints.print constraints
-(*
-  1) parse a file to generate constraints (for now: hardcoded file)
-  2) generate a cell grid with every cell being "all tiles"
-  3) select randomly a cell among those who have the lowest entropy
-  4) collapse it, if possible
-  5) propagate the constraints
-  6) repeat
-  7) write the output to disk, in a supertux-compatible way 
-*)
+  let constraint_map = Constraints.of_tilemap tilemap in
+  let wfc_grid = Grid.make 15 15 (Uncollapsed Tileset.all) in 
+  let rec wave_function_collapse () =
+    let min_entropy_cell = select_most_suitable_uncollapsed_cell wfc_grid in
+    (match min_entropy_cell with
+      | Some (y, x) -> (
+          collapse wfc_grid y x;
+          propagate_constraints constraint_map y x wfc_grid
+        )
+      | None -> failwith "Couldn't find a cell to collapse.");
+    match get_grid_state wfc_grid with
+      | Finished | Invalid_cell -> ()
+      | In_progress -> wave_function_collapse ()
+  in wave_function_collapse (); print_collapsed_grid wfc_grid
